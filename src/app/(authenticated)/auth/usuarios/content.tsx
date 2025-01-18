33a1f52763a691f9958f8ff3201016f1244ae9d2
vinicius <vinicius.scezar7@gmail.com>
2025-01-18 11:05:55 -0300

    fix: corrige erro de build na pagina de usuarios

--- src/app/(authenticated)/auth/usuarios/content.tsx
+++ src/app/(authenticated)/auth/usuarios/content.tsx
@@ -15,20 +15,24 @@ import {
   PaginationState,
 } from '@tanstack/react-table'
 
-import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { toast } from 'react-toastify'
+import { EllipsisVertical, User, ChevronLeft, ChevronRight } from 'lucide-react'
+import { type ColumnDef } from '@tanstack/react-table'
+import { useUpdateUser } from './_hooks/use-update-user'
 
+import { Checkbox } from '@/components/ui/checkbox'
+import { Dropdown } from '@/components/ui/dropdown-menu'
 import { Input } from '@/components/ui/input'
 import { Table } from '@/components/ui/table'
 import { Button } from '@/components/ui/button'
 
-import { UserPreview } from '@/services/user/types'
+import { type UserPreview } from '@/services/user/types'
 import { useGetAllStudents } from '@/hooks/services/use-get-all-students'
-
-import { columns } from './columns'
 
 export default function Content() {
   const { back } = useRouter()
   const { data: students } = useGetAllStudents()
+  const { mutate: handleUpdateUser } = useUpdateUser()
 
   const [pagination, setPagination] = useState<PaginationState>({
     pageIndex: 0,
@@ -40,6 +44,113 @@ export default function Content() {
   const [rowSelection, setRowSelection] = useState({})
 
   const data = (students ?? []) as UserPreview[]
+
+  const columns: ColumnDef<UserPreview>[] = (() => {
+    return [
+      {
+        id: 'select',
+        header: ({ table }) => (
+          <Checkbox
+            checked={
+              table.getIsAllPageRowsSelected() ||
+              (table.getIsSomePageRowsSelected() && 'indeterminate')
+            }
+            onCheckedChange={(value) =>
+              table.toggleAllPageRowsSelected(!!value)
+            }
+            aria-label="Select all"
+          />
+        ),
+        cell: ({ row }) => (
+          <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={(value) => row.toggleSelected(!!value)}
+            aria-label="Select row"
+          />
+        ),
+        enableHiding: false,
+        enableSorting: false,
+      },
+      {
+        accessorKey: 'name',
+        header: 'Estudante',
+        cell: ({ row }) => (
+          <div className="text-nowrap py-2 capitalize">
+            {row.getValue('name')}
+          </div>
+        ),
+      },
+      {
+        accessorKey: 'email',
+        header: 'E-mail',
+        cell: ({ row }) => (
+          <div className="text-nowrap py-2">{row.getValue('email')}</div>
+        ),
+      },
+      {
+        id: 'actions',
+        cell: ({ row }) => {
+          const userId = row.original.id
+          const userRole = row.original.role
+
+          return (
+            <div className="">
+              <Dropdown.Root>
+                <Dropdown.Trigger asChild className="cursor-pointer">
+                  <Button size="icon" variant="outline">
+                    <EllipsisVertical className="size-5" />
+                  </Button>
+                </Dropdown.Trigger>
+
+                <Dropdown.Content>
+                  <Dropdown.Sub>
+                    <Dropdown.SubTrigger>Designar função</Dropdown.SubTrigger>
+
+                    <Dropdown.SubContent>
+                      <Dropdown.Item
+                        onClick={() =>
+                          handleUpdateUser(
+                            {
+                              id: userId,
+                              data: { role: 'STUDENT' },
+                            },
+                            {
+                              onSuccess: () => {
+                                toast.success('Função designada com sucesso!')
+                              },
+                            },
+                          )
+                        }
+                      >
+                        Estudante
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        onClick={() =>
+                          handleUpdateUser(
+                            {
+                              id: userId,
+                              data: { role: 'PROFESSOR' },
+                            },
+                            {
+                              onSuccess: () => {
+                                toast.success('Função designada com sucesso!')
+                              },
+                            },
+                          )
+                        }
+                      >
+                        Professor(a)
+                      </Dropdown.Item>
+                    </Dropdown.SubContent>
+                  </Dropdown.Sub>
+                </Dropdown.Content>
+              </Dropdown.Root>
+            </div>
+          )
+        },
+      },
+    ]
+  })()
 
   const table = useReactTable({
     data,
